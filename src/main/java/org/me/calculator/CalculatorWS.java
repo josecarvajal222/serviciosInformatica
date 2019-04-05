9361aab0cd930b5f718c0182cbadf193d4b3322c
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-04-05 16:16:19 -0500

    creacion del web service

---
+++ src/main/java/org/me/calculator/CalculatorWS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.calculator;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author josec
+ */
+@WebService(serviceName = "CalculatorWS")
+@Stateless()
+public class CalculatorWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
