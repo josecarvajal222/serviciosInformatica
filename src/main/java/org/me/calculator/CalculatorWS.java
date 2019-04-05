eabc1f8cf8fc8bf8e734ef4b65d0664bb12f4bca
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-04-05 16:23:43 -0500

    Lógica del método add en ws

--- src/main/java/org/me/calculator/CalculatorWS.java
+++ src/main/java/org/me/calculator/CalculatorWS.java
@@ -19,19 +19,11 @@ import javax.ejb.Stateless;
 public class CalculatorWS {
 
     /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
-    }
-
-    /**
      * Web service operation
      */
     @WebMethod(operationName = "add")
     public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
-        //TODO write your implementation code here:
-        return 0;
-    }
+        int k = i + j;
+        return k;
+      }
 }
